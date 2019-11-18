daf3d03d05e6375ebb8bf366653822f49bc481da
Ross Dewberry <ross.dewberry@hotmail.com>
2019-11-18 11:22:44 -0800

    Update Project1Ross.m
    big change in how to calculate theta

--- Project1Ross.m
+++ Project1Ross.m
@@ -1,31 +1,27 @@
-%Bottom side of loop 1 on top side of loop 2
-
 l = 2;%Length of wire sides(cm)
 step = .0001;%Step size(cm)
 b = 0.5;%Speration of two rings
 mu0 = 4*pi*10^(-9);%Vacuum permeability
-x = b+l;
-y = 0;
-xp = 0;
-yp = 0;
+C = 10^(-9);
+x = b+l; %Initial loop 2 x value
+y = 0; %Initial loop 2 y value
+xp = 0; %Initial loop 1 x value
+yp = 0; %Intial loop 1 y value
 dM = 0;%Initialize infintesimal inductance
 M = 0;%Initialize inductance
 
-for t1 = 0:(pi/2):(3*pi/2)
+for t1 = 0:(pi/2):(3*pi/2) %Theta loop 1
     for l1 = 0:step:l %dl1
         xp = xp+(step*cos(t1)); %Loop 1 x position(cm)
         yp = yp+(step*sin(t1)); %Loop 1 y position (cm)
-        for t2 = 0:(pi/2):(3*pi/2)
+        for t2 = 0:(pi/2):(3*pi/2) %Theta loop 2
             for l2 = 0:step:l %dl2
                 x = x+(step*cos(t2)); %Loop 2 x position(cm)
                 y = y+(step*sin(t2)); %Loop 2 y positon(cm)
-                dM = ((step^2)*cos(t1-t2))/((x-xp)^2+(y-yp)^2)^(1/2); %Infintesimal inductance(1/cm)
-                M = M+(mu0/(4*pi))*dM; %Inductance(H)
+                theta = atan((y-yp)/(x-xp));
+                dM = ((step^2)*cos(theta))/(((x-xp)^2+(y-yp)^2)^(1/2)); %Infintesimal inductance(cm)
+                M = M+(C*dM); %Inductance(H)
             end
         end
     end
 end
-%M = 9.6166*10^-11  .1
-%M = 8.8145*10^-12  .01
-%M = 8.7347*10^-13  .001
-%M = 8.7267*10^-14  .0001
